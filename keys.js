42cf92d467366d597bbdb70ba8b5801e9c4e68eb
Adriana Garcia-Trevino <adrianagarcia-trevino@Adrianas-MacBook-Pro.local>
2017-10-06 21:30:58 -0500

    changes

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: '<Hnakb4EhUNp0IfTiinP64Vmug>',
+  consumer_secret: '<AyzKEt7wA8nD7QGNQJucL6r5UXyNcigJNFmgmcJpqtvidP7T3U>',
+  access_token_key: '<913210221803294725-rGEGUpB7qlsLfUqgYui0qIhpu7FR6KQ>',
+  access_token_secret: '<jIBLwFaslvZPGaRCpZwYkpCHDD7PaGqYstOx2wrRZN39I>',
+}
+var 
+module.exports = twitterKeys;
---
+++ omdbRequest.js
@@ -0,0 +1,10 @@
+// Basic Node application for requesting data from the OMDB we
+//Here we incorporate the "request" npm package
+var request = requeire ("request");
+
+//  We then run the rquest module on a URL with JSON
+request(http;)
+
+
+//If there were no errors and the response code was 200 (ie... the request was succesful)
+ if (!error)
